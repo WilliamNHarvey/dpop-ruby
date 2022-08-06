1e22656dd5b7dc744648802b37c7fa70a6b6bd49
WilliamNHarvey <williamnharvey@gmail.com>
2022-08-05 19:13:04 -0700

    Init

---
+++ lib/dpop.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "dpop/version"
+
+module Dpop
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dpop/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Dpop
+  VERSION = "0.0.1"
+end
