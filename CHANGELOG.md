4623098cb5f6436e22b4a2e10725a0dd7ed44871
WilliamNHarvey <williamnharvey@gmail.com>
2022-08-07 19:22:20 -0700

    workflow

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## v0.0.2
+## v0.1.0
  - Initial setup:
    - Configuration
    - Controller
--- lib/dpop/controller.rb
+++ lib/dpop/controller.rb
@@ -12,26 +12,41 @@ module Dpop
       end
     end
 
+    included do
+      class_attribute :ensure_dpop_on_actions
+      self.ensure_dpop_on_actions = false
+
+      # Set up the around_action which ensures the cookie is set on request if the controller has ensure_dpop! set
+      before_action :set_dpop_cookie      
+    end
+
     class_methods do
       def ensure_dpop!
-        return if cookie_jar[Dpop.config.cookie_name]
-
-        cookie_jar[Dpop.config.cookie_name] = Dpop::KeyGenerator.generate(Dpop.config.key_alg)
+        self.ensure_dpop_on_actions = true
       end
+    end
 
-      def get_proof(**args)
-        dpop_key = cookie_jar[Dpop.config.cookie_name]
-        raise MissingDpopCookie, Dpop.config.cookie_name unless dpop
+    def get_proof(**args)
+      dpop_key = cookie_jar[Dpop.config.cookie_name]
+      raise MissingDpopCookie, Dpop.config.cookie_name unless dpop_key
 
-        generator = Dpop::ProofGenerator.new(dpop_key, "RS256")
-        generator.create_dpop_proof(args)
-      end
+      generator = Dpop::ProofGenerator.new(dpop_key, "RS256")
+      generator.create_dpop_proof(args)
+    end
 
-      private
+    def set_dpop_cookie(&block)
+      return unless self.ensure_dpop_on_actions
+      return if cookie_jar[Dpop.config.cookie_name]
 
-      def cookie_jar
-        Dpop::CookieJar.new(Dpop.config.encryptor, request.cookies)
-      end
+      generated = Dpop::KeyGenerator.generate(Dpop.config.key_alg)
+
+      cookie_jar[Dpop.config.cookie_name] = generated
+    end
+
+    private
+
+    def cookie_jar
+      Dpop::CookieJar.new(Dpop.config.encryptor, request.cookies)
     end
   end
 end
